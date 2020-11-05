b557e889dd60dc4399d637597d4276ebf3013a8c
boohmax <boohmax@gmail.com>
2020-11-05 22:03:16 +0300

    update style

--- function.py
+++ function.py
@@ -1,13 +1,15 @@
-def select_bolts(data_bolts, data_nuts,
+def select_bolts(
+    data_bolts, data_nuts,
     data_washers, size=None, det1=None, det2=None,
-    thread='big', thread_entry='no', washer_head=1, nuts_count=2):
+    thread='big', thread_entry='no', washer_head=1, nuts_count=2
+):
     '''
 
     '''
 
     bolts_list = []
-    
-    if size is not None and det1 is not None and det2 is not None:    
+
+    if size is not None and det1 is not None and det2 is not None:
         for washer in data_washers:
             if washer['washer_size'] == size:
                 washer_width = washer['washer_width']
@@ -22,23 +24,23 @@ def select_bolts(data_bolts, data_nuts,
             if bolt['bolt_size'] == size:
                 length = washer_head*washer_width + det1 + det2
                 + washer_width + nuts_count*nut_width
-                
-                thread_inner = bolt['bolt_length']\
-                - washer_head*washer_width\
-                - det1 - det2\
-                - bolt['thread_length']
 
-                thread_outer = bolt['bolt_length']\
-                - washer_head*washer_width\
-                - det1 - det2\
-                - bolt['thread_length']
-              
+                thread_inner = (bolt['bolt_length']
+                                - washer_head*washer_width
+                                - det1 - det2
+                                - bolt['thread_length'])
+
+                thread_outer = (bolt['bolt_length']
+                                - washer_head*washer_width
+                                - det1 - det2
+                                - bolt['thread_length'])
+
                 if thread == "big":
                     thread_remain = 2 * bolt["thread_size_big"]
-                
+
                 if thread == "small" and bolt['thread_size_small'] != '':
                     thread_remain = 2 * bolt["thread_size_small"]
-          
+
                 if result:
                     result = (length + thread_remain) < bolt['bolt_length']
 
@@ -46,8 +48,10 @@ def select_bolts(data_bolts, data_nuts,
                     result = thread_inner > -5 and thread_inner > -det2/2
 
                 if result and thread_entry == 'no' or thread_entry == 'yes':
-                    result = thread_inner < 0 or\
-                    thread_outer > 0 and thread_outer < washer_width
+                    result = (
+                        thread_inner < 0 or thread_outer > 0 and
+                        thread_outer < washer_width
+                        )
 
                 if result and bolt['thread_length'] == bolt['bolt_length']:
                     bolt['thread_warning'] = 'резьба на всю длину болта'
